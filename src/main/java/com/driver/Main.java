0a1582b78883951bfb42524e7c75014639158225
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-06-20 13:53:26 +0000

    first comit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-  
+  class A {
+    public static String meth(){
+        return "Invoking method from class A";
+    }
+  }
+  class B extends A {
+    public static String meth() {
+        return "Method is overridden in Extended class B";
+    }
+  }
+
+  public static void main(String args[]){
+    B obj = new B();
+     String result = obj.meth();
+        System.out.println(result);
+  }
 }
